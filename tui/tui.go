615ec8e1d6bcee9e95fdcecb122cc99c9dfaa11e
Andrew Vota <andrew@vota.cc>
2024-06-18 11:38:29 -0400

    feat: Root cobra command
    feat: Base `bubbletea` model

---
+++ tui/tui.go
@@ -0,0 +1,50 @@
+package tui
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	keys keyMap
+}
+
+func NewTui() Model {
+	return Model{
+		keys: defaultKeyMap(),
+	}
+}
+
+type keyMap struct {
+	Quit key.Binding
+}
+
+func defaultKeyMap() keyMap {
+	return keyMap{
+		Quit: key.NewBinding(
+			key.WithKeys("ctrl+c"),
+		),
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keys.Quit):
+			return m, tea.Quit
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m Model) View() string {
+	return ""
+}
