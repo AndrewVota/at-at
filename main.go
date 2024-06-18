1c148f4879f2be054dd86fbc3d9514ee75358eab
Andrew Vota <andrew@vota.cc>
2024-06-18 13:18:56 -0400

    feat: Run root `cmd` in main

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/andrewvota/at-at/cmd"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	cmd.Execute()
+}
